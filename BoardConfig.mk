a53abbaa0ae24b877b428b925d60c9c02da912c9
buddi56 <30726378+buddi56@users.noreply.github.com>
2020-01-02 06:40:56 +0530

    added two flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@
 
 LOCAL_PATH := device/oppo/CPH1861
 
-#system-as-root
+# system-as-root (SAR) and OTA Assert
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 TARGET_OTA_ASSERT_DEVICE := CPH1861
@@ -64,6 +64,9 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 buildvariant=user androidboot.sel
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
 
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x14f88000 --tags_offset 0x13f88000 --header_version 1
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
 
 # Platform
 
@@ -130,6 +133,7 @@ TW_DEFAULT_BRIGHTNESS := 365
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TW_USE_TOOLBOX := true
+TW_SCREEN_BLANK_ON_BOOT := false
 TW_NO_SCREEN_BLANK := true
 TW_NO_BATT_PERCENT := false
 TW_NO_USB_STORAGE := false
@@ -139,13 +143,12 @@ TW_EXTRA_LANGUAGES := true
 TW_DEFAULT_LANGUAGE := en
 TW_OZIP_DECRYPT_KEY := "172B3E14E46F3CE13E2B5121CBDC4321"
 
-#OS
+# OS, Encryption
 
 PLATFORM_SECURITY_PATCH := 2019-12-05
 BOARD_OS_VERSION := 9.0.0
-
-# Encryption
-
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE := true
 TW_CRYPTO_FS_TYPE := "ext4"
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
 TW_CRYPTO_MNT_POINT := "/data"
