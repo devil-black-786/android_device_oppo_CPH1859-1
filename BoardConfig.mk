a7e7db3101a70d248e22c474c4471e22de07198a
devil-black-786 <60068093+devil-black-786@users.noreply.github.com>
2020-08-29 13:54:43 +0530

    Added FBE support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,11 +158,7 @@ PLATFORM_SECURITY_PATCH := 2020-02-05
 BOARD_OS_VERSION := 9.0.0
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_FBE := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
-
+TW_INCLUDE_CRYPTO_FBE := true
 # Debug
 
 TWRP_INCLUDE_LOGCAT := true
